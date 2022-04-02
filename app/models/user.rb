7dd45e7ef82b44b2cfa9f0b93de0f837bdc35f36
binarygit <inscription4@outlook.com>
2022-04-02 17:35:53 +0545

    add validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
