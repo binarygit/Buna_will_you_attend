2298ca33ef40d99d34b73755d558c2552b73bd75
binarygit <inscription4@outlook.com>
2022-04-04 18:05:51 +0545

    add associations between user and access level

--- app/models/access_level.rb
+++ app/models/access_level.rb
@@ -1,2 +1,3 @@
 class AccessLevel < ApplicationRecord
+  has_many :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  belongs_to :access_level
 
   validates :username, :email, presence: true, uniqueness: true
   validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, :access_level_id, presence: true
 end
