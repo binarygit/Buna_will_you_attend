0b6fb400abb9a95f59d196fe95432dbecc84a367
binarygit <inscription4@outlook.com>
2022-04-04 18:08:55 +0545

    create some fixtures and test username validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should not save user without username' do
+    user = User.new
+    assert_not user.save
+  end
 end
