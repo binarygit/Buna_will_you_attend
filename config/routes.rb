232869ddae550c3c41325b2edd34aaa05387d5c2
binarygit <inscription4@outlook.com>
2022-04-07 17:06:57 +0545

    add path for new to sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'events#index'
-  resources :sessions, only: [:create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
