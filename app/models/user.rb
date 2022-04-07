9c0fb949626cdda4827e1b76b0c36b5c6e679b0b
binarygit <inscription4@outlook.com>
2022-04-07 17:05:10 +0545

    add access_level to a user before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,22 @@ class User < ApplicationRecord
   has_secure_password
   belongs_to :access_level
 
+  before_validation :set_access_level
+
   validates :username, :email, presence: true, uniqueness: true
   validates :password, confirmation: true
   validates :password_confirmation, :access_level_id, presence: true
+
+  private
+
+  def set_access_level
+    privileged ||= AccessLevel.where(name: 'privileged').first.id
+    ordinary ||= AccessLevel.where(name: 'ordinary').first.id
+
+    if email.eql?('buna@buna.com')
+      self.access_level_id = priviledged
+    else
+      self.access_level_id = ordinary
+    end
+  end
 end
