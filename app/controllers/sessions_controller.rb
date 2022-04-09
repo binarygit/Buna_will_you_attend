65042a153439e0a37381a9b3fbd6e4664f62ff48
binarygit <inscription4@outlook.com>
2022-04-09 10:37:00 +0545

    create session#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,23 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'You are successfully logged in'
+    else
+      flash.now[:alert] = 'Email or password is invalid'
+      render :new
+    end
+  end
+
   def destroy
+  end
+
+  private 
+
+  def form_params 
+    params.permit(:email, :password)
   end
 end
