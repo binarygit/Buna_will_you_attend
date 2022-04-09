222efff6de74fd5a6c6bc8c0dc9b87158f7c61df
binarygit <inscription4@outlook.com>
2022-04-09 09:34:33 +0545

    create login/logout, signup, events routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root 'events#index'
+
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
