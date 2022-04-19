4e0b33d561b64432be12230eab4846db8ccff26e
binarygit <inscription4@outlook.com>
2022-04-19 15:11:58 +0545

    create helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
+  def logged_in?
+    true if @current_user
+  end
 end
