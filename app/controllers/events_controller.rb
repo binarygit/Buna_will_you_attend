da457f114079ebfdce07f1d77545898a7b2ff443
binarygit <inscription4@outlook.com>
2022-05-05 14:39:52 +0545

    let users create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,25 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
     @event = Event.new
+  end
+
+  def create
+    @event = Event.new(form_params)
+
+    if @event.save
+      redirect_to root_path, notice: 'Event successfully created!'
+    else
+      render :new, status: :internal_server_error
+    end
+  end
+
+  private
+
+  def form_params
+    params.require(:event).permit(:title, :description)
   end
 end
