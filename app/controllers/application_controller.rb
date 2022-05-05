3b0f954da5c0ccff9f7b3c13bb947fe6f08817f1
binarygit <inscription4@outlook.com>
2022-05-05 16:36:30 +0545

    add create event to nav and authenticate user before accessing other event methods expect index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def authenticate_user
+    unless logged_in?
+      redirect_to root_path, alert: 'You need to login first!'
+    end
+  end
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
