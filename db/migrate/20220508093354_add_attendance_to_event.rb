f9538d24dc7b8e002bd9f1c336c64ce67c2304f8
binarygit <inscription4@outlook.com>
2022-05-08 15:22:08 +0545

    create an attendance column for event

---
+++ db/migrate/20220508093354_add_attendance_to_event.rb
@@ -0,0 +1,5 @@
+class AddAttendanceToEvent < ActiveRecord::Migration[7.0]
+  def change
+    add_column :events, :attendance, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_08_080612) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_08_093354) do
   create_table "access_levels", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_08_080612) do
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.string "venue"
+    t.boolean "attendance"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
