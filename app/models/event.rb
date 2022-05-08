ab2a4c2614a263b237cdf19caa48f01e5ad6a95f
binarygit <inscription4@outlook.com>
2022-05-08 12:23:38 +0545

    make date a mandatory field when creating events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true, length: {minimum: 100}
+  validates :title, :date, presence: true
+  validates :description, presence: true, length: {minimum: 20}
 end
