af75db0827b4bfbb2693d9c58e229842438a642d
binarygit <inscription4@outlook.com>
2022-05-08 11:55:58 +0545

    test whether visitor can create events

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -5,24 +5,30 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   
-  test 'should create event' do
+  test 'should create event when user is logged in' do
+    user = users(:one)
+    post sessions_url, params: { email: 'thulomama@thulomama.com', password: 'secret'}
+
     assert_difference 'Event.count' do
       post events_url, params: { event: {
                                    title: 'New Article',
                                    description: 'I am not sure right now that a minimum restriction on descriptions should be imposed.' +
-                                                'Also I feel like a hundred words is a bit too much'
+                                                'Also I feel like a hundred words is a bit too much' +
+                                                'I might reduce the minimum requirement or do away with it altogether'
       }}
     end
 
     assert_equal 'Event successfully created!', flash.notice
   end
 
-  test 'should not create event with terse description' do
+  test 'should not create event when user is not logged in' do
     assert_no_difference 'Event.count' do
       post events_url, params: { event: {
                                    title: 'New Article',
                                    description: 'I am not sure right now that a minimum restriction on descriptions should be imposed.'
       }}
+
+      assert_equal 'You need to login first!', flash[:alert]
     end
   end
 end
